365874315273bd9f9448e3254f6f004a41c0ae98
Zack Zhao <zackzhao@nnti.com>
2017-05-31 09:51:52 +0800

    first commit

---
+++ gowiki/wiki.go
@@ -0,0 +1,36 @@
+/*wiki geneate page file named its title and print page body
+okay!
+*/
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+)
+
+// Page place title and body in it
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage(p1.Title)
+	fmt.Println(string(p2.Body))
+}
